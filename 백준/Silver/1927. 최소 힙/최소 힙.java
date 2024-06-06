71f64f9b040ff2975e6ac3a654c1b002e9466974
chobeebee <97503991+chobeebee@users.noreply.github.com>
2024-06-06 19:55:08 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 26520 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         int num = Integer.parseInt(br.readLine());
 
@@ -15,13 +16,14 @@ public class Main {
 
             if (value == 0) {
                 if (minHeap.isEmpty()) {
-                    System.out.println("0");
+                    sb.append(0).append("\n");
                 }else{
-                    System.out.println(minHeap.poll());
+                    sb.append(minHeap.poll()).append("\n");
                 }
             }else{
                 minHeap.add(value);
             }
         }
+        System.out.println(sb);
     }
 }
