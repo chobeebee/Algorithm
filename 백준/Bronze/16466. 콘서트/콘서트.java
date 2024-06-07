41f6ce247fe4ee6e39fdb07638b6adc3a8bcd030
chobeebee <97503991+chobeebee@users.noreply.github.com>
2024-06-07 16:48:28 +0900

    [Bronze I] Title: 콘서트, Time: 600 ms, Memory: 96588 KB -BaekjoonHub

---
+++ 백준/Bronze/16466. 콘서트/콘서트.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine());
+        int[] sold = new int[num];
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < num; i++){
+            sold[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, sold[i]);
+        }
+
+        Arrays.sort(sold);
+
+        int count = 1;
+        for(int i = 0; i < num; i++){
+            if(sold[i] != i+1){
+                break;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
