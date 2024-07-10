fc2ae1946aeb8ff64fdb47afdbe188df5e707513
chobeebee <97503991+chobeebee@users.noreply.github.com>
2024-07-10 22:56:52 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.13 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for(int i=0; i<queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for(int j=s; j<=e; j++){
+                if(j%k == 0){
+                    arr[j]+=1;
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
