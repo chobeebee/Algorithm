fbfd76e44bbf77a4e5fa887f24b655a213529584
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-07-29 01:51:10 +0900

    [level 0] Title: 5명씩, Time: 16.21 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<names.length; i++){
+            if(i%5==0){
+                answer.add(names[i]);
+            }
+        }
+        
+        return answer.stream().map(s->s).toArray(String[]::new);
+    }
+}
