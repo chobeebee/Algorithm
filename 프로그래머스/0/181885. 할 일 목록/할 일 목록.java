2bd77bc65eec276e2ea8a041ae12a154efcab26e
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-07-29 01:56:40 +0900

    [level 0] Title: 할 일 목록, Time: 2.30 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<todo_list.length; i++){
+            if(!finished[i]){
+                answer.add(todo_list[i]);
+            }
+        }
+        
+        return answer.stream().map(s->s).toArray(String[]::new);
+    }
+}
