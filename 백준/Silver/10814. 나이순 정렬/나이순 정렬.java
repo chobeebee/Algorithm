64cc7c45eea3f2806d8eb731479b2037c0ad85f1
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-08-08 21:44:58 +0900

    [Silver V] Title: 나이순 정렬, Time: 744 ms, Memory: 59520 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -10,6 +10,7 @@ import static java.lang.System.in;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
         String[][] arr = new String[N][2];
@@ -28,7 +29,8 @@ public class Main {
         });
 
         for(int i = 0; i < N; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
         }
+        System.out.println(sb);
     }
 }
