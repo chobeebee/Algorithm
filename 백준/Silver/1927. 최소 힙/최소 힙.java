64355c1fe681ead151fbb9edc4b6c2cc776f67bb
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-09-20 21:33:15 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26104 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -3,9 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class Main {
+import static java.lang.System.in;
 
+public class Main {
     public static void main(String[] args) throws IOException {
+        /**
+         * 배열에 자연수 X를 넣음
+         * 배열에서 가장 작은 값 출력, 그 값을 배열에서 제거
+         * 연산의 개수 1<=N<=100,000
+         * 정수x가 자연수면 배열에 추가, 0이면 배열에서 가장 작은 값 출력 후 배열에서 제거
+         * */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb= new StringBuilder();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
