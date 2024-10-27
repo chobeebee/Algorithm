d4e51a1983e31e7d71d3a55d1ef4c1180f498647
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-10-27 23:31:25 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.18 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i][j] == arr[j][i]){
+                    answer++;
+                }
+            }
+        }
+        
+        answer = (answer == arr.length*arr.length) ? 1: 0;
+        
+        return answer;
+    }
+}
