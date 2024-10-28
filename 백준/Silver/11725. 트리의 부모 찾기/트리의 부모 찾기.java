bc043971bdb4e5ea2be37f6576fc47e1286430d6
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-10-28 21:55:12 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1144 ms, Memory: 71956 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -8,6 +8,11 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N;
+    static ArrayList<ArrayList<Integer>> tree;
+    static boolean[] visited;
+    static int[] parent;
+
     public static void main(String[] args) throws IOException {
         /**
          * 트리의 루트 1일 때, 각 노드의 부모를 구하시오.
@@ -27,10 +32,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine()); //노드 개수
-        ArrayList<ArrayList<Integer>> tree = new ArrayList<>(); //노드 관계 맵
-        boolean[] visited = new boolean[N+1]; //방문 체크
-        int[] parent = new int[N+1]; //부모 노드 저장 배열
+        N = Integer.parseInt(br.readLine()); //노드 개수
+        tree = new ArrayList<>(); //노드 관계 맵
+        visited = new boolean[N+1]; //방문 체크
+        parent = new int[N+1]; //부모 노드 저장 배열
 
         //맵 초기화
         for (int i = 1; i <= N+1; i++) {
@@ -47,9 +52,13 @@ public class Main {
         }
 
         //bfs
+        bfs(1); //루트 1
+    }
+
+    private static void bfs(int root) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(1); //루트 1
-        visited[1] = true;
+        queue.add(root); //루트 1
+        visited[root] = true;
 
         while (!queue.isEmpty()) {
             int p = queue.remove();
