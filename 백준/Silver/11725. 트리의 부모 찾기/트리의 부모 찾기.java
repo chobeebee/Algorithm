5a9879f3426ea97e69a80eef21efe449ba576c67
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-10-28 21:51:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1284 ms, Memory: 76736 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /**
+         * 트리의 루트 1일 때, 각 노드의 부모를 구하시오.
+         * 2<= 노드 개수 N <=100,000
+         * N-1개의 줄에 연결된 두 정점
+         * 2번 노드부터 순서대로 출력
+         *
+         *     1
+         *    ㅅ
+         *   4   6
+         *  /\   \
+         *  2 7   3
+         *        \
+         *         5
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine()); //노드 개수
+        ArrayList<ArrayList<Integer>> tree = new ArrayList<>(); //노드 관계 맵
+        boolean[] visited = new boolean[N+1]; //방문 체크
+        int[] parent = new int[N+1]; //부모 노드 저장 배열
+
+        //맵 초기화
+        for (int i = 1; i <= N+1; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        //노드 연결 입력
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            tree.get(node1).add(node2);
+            tree.get(node2).add(node1);
+        }
+
+        //bfs
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1); //루트 1
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int p = queue.remove();
+
+            for (int node : tree.get(p))
+                if (!visited[node]) {
+                    visited[node] = true;
+                    queue.add(node);
+                    parent[node] = p; // 부모 노드 찾은 후 저장
+                }
+        }
+
+        // 2번 노드부터 부모 노드 출력
+        for (int i = 2; i <= N; i++){
+            System.out.println(parent[i]);
+        }
+    }
+}
