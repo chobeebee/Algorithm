e5386bc2f371d109eebd61f4251df8b7867e61af
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-11-06 10:50:49 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n) {
-        if(n==1) return 1;
+        if(n <= 7) return 1;
+        
         int answer = n % 7 == 0 ? n/7 : (n/7) + 1;
         
         return answer;
