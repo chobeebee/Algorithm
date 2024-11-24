f52e1a9df38a1f1eaac8475c935995b9a6e58781
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-11-24 21:59:32 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.37 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        my_string = my_string.replaceAll("[a-zA-Z]", "");
+        
+        for(int i=0; i<my_string.length(); i++){
+            answer += my_string.charAt(i) - '0';
+        }
+        
+        return answer;
+    }
+}
