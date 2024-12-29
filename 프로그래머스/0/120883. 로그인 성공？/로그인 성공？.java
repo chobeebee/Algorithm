cbc86255375af9676d680aacc895e031ed68e8a2
조수빈 <97503991+chobeebee@users.noreply.github.com>
2024-12-29 23:19:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        
+        for(int i=0; i<db.length; i++){
+            if(db[i][0].equals(id_pw[0])){
+                if(db[i][1].equals(id_pw[1])){
+                    return "login";
+                }
+                return "wrong pw";
+            }
+        }
+        
+        return answer;
+    }
+}
