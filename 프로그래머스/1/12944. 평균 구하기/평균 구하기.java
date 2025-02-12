178e15212b5c6cb8030ec34c0b2dad90118d1d58
조수빈 <97503991+chobeebee@users.noreply.github.com>
2025-02-12 14:06:43 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,14 +1,12 @@
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
+
         int sum = 0;
         
         for(int a : arr){
             sum += a;
         }
         
-        answer = (double)sum/arr.length;
-        
-        return answer;
+        return (double)sum/arr.length;
     }
 }
