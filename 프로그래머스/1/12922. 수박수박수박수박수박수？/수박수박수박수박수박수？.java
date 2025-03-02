aba16600e89c5cd238fc5386c2db400f1e7e3400
조수빈 <97503991+chobeebee@users.noreply.github.com>
2025-03-02 16:42:44 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 46.49 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -3,11 +3,7 @@ class Solution {
         String answer = "";
         
         for(int i=1; i<=n; i++){
-            if(i%2 == 1){
-                answer+="수";
-            }else{
-                answer+="박";
-            }
+            answer += (i%2 == 1) ? "수" : "박";
         }
         
         return answer;
