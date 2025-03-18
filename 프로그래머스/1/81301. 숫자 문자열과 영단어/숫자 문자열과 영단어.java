460626b4d0b3eef578c9af43db0d47276b5d0f89
조수빈 <97503991+chobeebee@users.noreply.github.com>
2025-03-18 10:36:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.54 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,38 +4,15 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        Map<String, Integer> wordToNum = new HashMap<>();
         String[] words = {"zero", "one", "two", "three", "four", "five","six","seven","eight","nine"};
-        String tmp = "";
-        String num = "";
         
-        //맵 단어 세팅
+        //문자열에 일치하는 단어가 있다면 자리 번호로 교체
         for(int i=0; i<words.length; i++){
-            wordToNum.put(words[i], i);
+            s = s.replaceAll(words[i], Integer.toString(i));
         }
         
-        //단어 탐색
-        for(int i=0; i<s.length(); i++){
-            
-            char c = s.charAt(i);
-            
-            //숫자이면 바로 추가
-            if(Character.isDigit(c)){
-                tmp += c;
-            }else{
-                
-                //단어가 될 때까지 진행 후, 단어에 해당하는 숫자를 찾아서 추가
-                num += c;
-                
-                if(wordToNum.containsKey(num)){
-                    tmp += wordToNum.get(num);
-                    num = "";
-                }                   
-                
-            }
-        }
-        
-        answer = Integer.parseInt(tmp); //문장을 숫자로 변환
+        //문장을 숫자로 변환
+        answer = Integer.parseInt(s);
         
         
         return answer;
