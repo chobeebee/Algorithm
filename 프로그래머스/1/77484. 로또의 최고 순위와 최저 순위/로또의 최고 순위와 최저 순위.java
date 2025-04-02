045a665b473adcf8f1014b9723445f30152e36ce
조수빈 <97503991+chobeebee@users.noreply.github.com>
2025-04-02 10:43:03 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int zero = 0; //0 개수
+        int match = 0; //일치하는 숫자
+        
+        //번호 탐색
+        for(int lott : lottos){
+            if(lott == 0){
+                zero++;
+            }else{
+                for(int win : win_nums){
+                    if(lott == win){
+                        match++;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        int maxRank = Math.min(7-(match+zero), 6); //최고 등수
+        int minRank = Math.min(7-match, 6); //최저 등수
+        int[] answer = {maxRank, minRank};
+        
+        return answer;
+    }
+}
