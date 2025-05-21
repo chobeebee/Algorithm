4e378ad96c258b83e092e7ceb5c9c88828fecd44
조수빈 <97503991+chobeebee@users.noreply.github.com>
2025-05-21 23:15:43 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
     /*
@@ -14,17 +13,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = br.readLine().split(""); // 주어진 수를 문자 배열로 받기
-        List<Integer> list = new ArrayList<>();
         
-        // 정렬을 위해 리스트에 각 숫자 넣기
-        for (String s : arr) {
-            list.add(Integer.parseInt(s));
-        }
         // 내림차순 정렬
-        list.sort(Collections.reverseOrder());
-        // 리스트를 문자로 이어서 출력
-        for(int l : list){
-            System.out.print(l+"");
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        // 배열 출력
+        for(String a : arr){
+            System.out.print(a);
         }
     }
 }
